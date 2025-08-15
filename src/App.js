0e5da551c9385b8db4d79b8b386bd04aa64f14d2
Sophie-Ndobe <139694211+Sophie-Ndobe@users.noreply.github.com>
2025-08-15 21:05:49 +0200

    Created the project structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-primary">📖📖The English Dictionary📖📖</h1>
+    </div>
+  );
+}
+
+export default App;
