2c58bdaebe405e4e6b1b52803638db5281b24c8c
Sophie-Ndobe <139694211+Sophie-Ndobe@users.noreply.github.com>
2025-08-18 16:19:33 +0200

    Added a footer to the project

--- src/App.js
+++ src/App.js
@@ -7,6 +7,33 @@ function App() {
       <div className="container">
         <h1 className="text-center">📖📖The English Dictionary📖📖</h1>
         <Dictionary />
+        <footer className="text-center">
+          This project was developed by{" "}
+          <a
+            href="https://github.com/Sophie-Ndobe?tab=repositories"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sophie Ndobe
+          </a>
+          , released as{" "}
+          <a
+            href="https://github.com/Sophie-Ndobe/my-english-dictionary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open source
+          </a>
+          , and is hosted on{" "}
+          <a
+            href="https://english-ditionary-react-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>{" "}
+          .
+        </footer>
       </div>
     </div>
   );
